dcf9d3f13b21e59f5ba1e55d444136867427c109
Tu Luong <luongduc.tu1992@gmail.com>
2017-03-17 20:22:01 -0400

    Worked on Proposal

--- design/app/scripts/main.js
+++ design/app/scripts/main.js
@@ -11,13 +11,13 @@ var uberData = {
 	'sep14' : 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv',
 };
 
-var drawMap = function(mapData, data, key, htmlID) {
+var drawMap = function(mapData, data, htmlID, color, lon, lat) {
 
     var container = $(htmlID);
 
-    data = _.chunk(data, 100000)[0];
+    // data = _.chunk(data, 25000)[0];
 
-    console.log(data);
+    // console.log(data);
 
     // data.map(function(d) {
     // 	var day = moment(d["Date/Time"]).day();
@@ -29,8 +29,8 @@ var drawMap = function(mapData, data, key, htmlID) {
 	    height = container.height(),
 	    center = d3.geoCentroid(mapData),
 	    offset = [width / 2, height / 2],
-	    scale = 75000,
-	    precision = 0.01
+	    scale = 125000,
+	    precision = 0.1
 
     var svg = d3.select(htmlID)
     		    .append('svg')
@@ -38,7 +38,7 @@ var drawMap = function(mapData, data, key, htmlID) {
     			    .attr('height', height);
 
 	// var color = d3.scaleSequential(d3.interpolateReds);
-	var color = d3.scaleOrdinal(d3.schemeCategory20);
+	// var color = d3.scaleOrdinal(d3.schemeCategory20);
 	// var color = d3.scaleOrdinal(d3.schemePastel1);
 
 	/* map projection */
@@ -87,21 +87,34 @@ var drawMap = function(mapData, data, key, htmlID) {
 		        	// return (_.isNil(playersCount[d.properties.name])) ? "" : d.properties.name + ": " + playersCount[d.properties.name];
 		    	});
 
+	// plotting points
     svg.selectAll('circle')
 		.data(data)
 		.enter().append('circle')
-		.attr('cx', function (d) { return projection([d.Lon, d.Lat])[0]; })
-		.attr('cy', function (d) { return projection([d.Lon, d.Lat])[1]; })
 		.attr('r', '0.5px')
-		.attr('fill', 'red')
+		// .attr('cx', function (d) { return projection([d.Lon, d.Lat])[0]; })
+		// .attr('cy', function (d) { return projection([d.Lon, d.Lat])[1]; })
+		.attr("transform", function(d) {
+		            return "translate(" + projection([d[lon], d[lat]]) + ")"; })
+		            // return "translate(" + projection([d.Lon, d.Lat]) + ")"; })
+		.attr('fill', color);
 }
 
-var dataViz = function(errors, mapData, fhvBases, zones, uberApr14) {
+var dataViz = function(errors, mapData, fhvBases, zones, uberSep14, lyft14) {
 
     if (errors) throw errors;
 
-    drawMap(mapData, uberApr14, null, '#nyc');
+    // lyft14 = _.orderBy(lyft14, ["time_of_trip"], ["asc"]);
+    lyft14 = _.orderBy(_.filter(lyft14, function(d) { return (moment(new Date(d.time_of_trip)).month() === 8 && moment(new Date(d.time_of_trip)).date() === 1); }), ["time_of_trip"], ["asc"]);
+    uberSep14 = _.orderBy(_.filter(uberSep14, function(d) { return (moment(new Date(d["Date/Time"])).date() === 1); }), ["Date/Time"], ["asc"]);
 
+    // console.log(lyft14);
+    // console.log(uberSep14);
+
+    drawMap(mapData, lyft14, '#nyc-lyft', "red", "start_lng", "start_lat");
+    drawMap(mapData, uberSep14, '#nyc-uber', "red", "Lon", "Lat");
+
+    // console.log(moment(lyft14[0]["time_of_trip"]).month() === 8);
 }
 
 d3.queue()
@@ -110,5 +123,6 @@ d3.queue()
     // .defer(d3.json, "https://gist.githubusercontent.com/mbostock/8423351/raw/fb182232861027a6adecf8b2182ee7b3ef41c751/ny.json")
     .defer(d3.csv, 'https://raw.githubusercontent.com/toddwschneider/nyc-taxi-data/master/data/fhv_bases.csv')
     .defer(d3.csv, 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/taxi-zone-lookup.csv')
-    .defer(d3.csv, 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-apr14.csv')
+    .defer(d3.csv, 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv')
+    .defer(d3.csv, 'https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/other-FHV-data/Lyft_B02510.csv')
     .await(dataViz);
