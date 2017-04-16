7f5f23f5c980dc4c241690b58fe5b520ec1f9048
Tu Luong <luongduc.tu1992@gmail.com>
2017-04-16 03:02:39 -0400

    Worked on design 2

--- design/app/scripts/main.js
+++ design/app/scripts/main.js
@@ -1,7 +1,7 @@
-var drawMap = function(mapData, data, zones, htmlID) {
+var drawMap = function(mapData, data, totalData, htmlID, totalLabelID) {
 
     var container = $(htmlID);
-    var totalUberPickups = $("#totalUberPickups");
+    var totalPickups = $(totalLabelID);
 
     // var data = _.orderBy(_.filter(data, function(d) {
     //     return (moment(new Date(d['Pickup_date'])).month() === month.key); }), ['Pickup_date'], ['asc']);
@@ -22,15 +22,24 @@ var drawMap = function(mapData, data, zones, htmlID) {
     // var dataCount = _.orderBy(_.map(_.countBy(data, "ntacode"), function(value, key) { return { key: key, value: value }; }), "value", "desc");
     var maxData = _.maxBy(data, function(o) { return o.value; });
 
-    var total = numeral(_.sumBy(data, 'value')).format();
-    totalUberPickups.html(total);
+    var total = 0;
+    _.each(mapData.features, function(d) {
+        _.each(data, function(o) {
+            if (o.key == d.properties.NTACode) {
+                total += o.value;
+            }
+        });
+    });
+
+    totalPickups.html(numeral(total).format());
 
     /* draw map */
     var width = container.width(),
         height = container.height(),
         center = d3.geoCentroid(mapData),
         offset = [width / 2, height / 2],
-        scale = 75000,
+        // scale = 75000,
+        scale = 150000,
         precision = 0.1;
 
     var svg = d3.select(htmlID)
@@ -102,7 +111,7 @@ var drawMap = function(mapData, data, zones, htmlID) {
 
 }
 
-var dataViz = function(errors, mapData, fhvBases, zones, uber1, uber2, uber3, uber4, uber5, uber6) {
+var dataViz = function(errors, mapData, fhvBases, zones, uber1, uber2, uber3, uber4, uber5, uber6, fhv1, fhv2, fhv3, fhv4, fhv5, fhv6) {
 
     if (errors) throw errors;
 
@@ -116,22 +125,47 @@ var dataViz = function(errors, mapData, fhvBases, zones, uber1, uber2, uber3, ub
     ];
 
     var uber = countValues(_.union(uber1, uber2, uber3, uber4, uber5, uber6), "value");
+    var fhv = countValues(_.union(fhv1, fhv2, fhv3, fhv4, fhv5, fhv6), "value");
+    var total = countValues(_.union(uber, fhv), "value");
+
+    console.log(total);
 
     // lyft = _.orderBy(lyft, ["time_of_trip"], ["asc"]);
     // lyft = _.orderBy(_.filter(lyft, function(d) { return (moment(new Date(d.time_of_trip)).month() === 8 && moment(new Date(d.time_of_trip)).date() === 1); }), ['time_of_trip'], ['asc']);
 
-    // var manhattan = _.filter(mapData.features, function(d) {
-    //  return d.properties.borough === "Manhattan";
-    //  return d.properties.BoroName === "Manhattan";
-    // });
+    var manhattan = _.filter(mapData.features, function(d) {
+        // return d.properties.borough === "Manhattan";
+        return d.properties.BoroName === "Manhattan";
+    });
 
-    // mapData = {
-    //  "type": "FeatureCollection",
-    //  "features": manhattan
-    // };
+    mapData = {
+        "type": "FeatureCollection",
+        "features": manhattan
+    };
 
     // drawMap(mapData, lyft, '#nyc-lyft', 'red', 'start_lng', 'start_lat');
-    drawMap(mapData, uber, zones, '#nyc-uber');
+    // drawMap(mapData, uber, zones, '#nyc-uber');
+
+    // fhv = fhv.map(function(d) {
+    //     var ntacode = "";
+    //     zones.map(function(z) {
+    //         if (z.location_id === d.locationID &&
+    //             z.location_id != 264 &&
+    //             z.location_id != 265) {
+    //             ntacode = z.ntacode;
+    //         }
+    //     });
+    //     d["ntacode"] = ntacode;
+    //     return d;
+    // });
+    // fhv = fhv.filter(function(d) {
+    //     if (d.locationID != "" && d.ntacode != "") {
+    //         return d;
+    //     }
+    // });
+
+    drawMap(mapData, uber, total, '#nyc-uber', "#totalUberPickups");
+    drawMap(mapData, fhv, total, '#nyc-fhv', "#totalFHVPickups");
 }
 
 var countValues = function(data) {
@@ -156,6 +190,12 @@ d3.queue()
     .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/uber/uber-jan-june15/apr.json')
     .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/uber/uber-jan-june15/may.json')
     .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/uber/uber-jan-june15/jun.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-01.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-02.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-03.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-04.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-05.json')
+    .defer(d3.json, 'https://raw.githubusercontent.com/se2/cis602-02-project/master/data/fhv/jan.jun.15/fhv_2015-06.json')
     // .defer(d3.csv,   'https://media.githubusercontent.com/media/se2/cis602-02-project/master/data/uber/uber-sep14.csv')
     // .defer(d3.csv,   'https://media.githubusercontent.com/media/se2/cis602-02-project/master/data/fhv/Lyft_B02510.csv')
     .await(dataViz);
