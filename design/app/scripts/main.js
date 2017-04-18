db5bc2d109a063e4293f72d93de063c5599ff0e4
Tu Luong <luongduc.tu1992@gmail.com>
2017-04-17 22:43:11 -0400

    Update design 3

--- design/app/scripts/main.js
+++ design/app/scripts/main.js
@@ -1,4 +1,4 @@
-var drawMap = function(mapData, scale, data, totalData, htmlID, totalLabelID) {
+var drawMap = function(mapData, scale, rightMargin, data, totalData, htmlID, totalLabelID) {
 
     var container = $(htmlID);
     var totalPickups = $(totalLabelID);
@@ -9,7 +9,7 @@ var drawMap = function(mapData, scale, data, totalData, htmlID, totalLabelID) {
     totalPickups.html(numeral(total).format());
 
     /* draw map */
-    var width = container.width(),
+    var width = container.width() - rightMargin,
         height = container.height(),
         center = d3.geoCentroid(mapData),
         offset = [width / 2, height / 2],
@@ -45,7 +45,7 @@ var drawMap = function(mapData, scale, data, totalData, htmlID, totalLabelID) {
                     colorScale = (o.value / maxData.value);
                 }
             });
-            return (colorScale == 0) ? "white" : color(colorScale);
+            return (colorScale == 0) ? 'white' : color(colorScale);
         })
         .attr('d', path)
         .attr('stroke', 'grey')
@@ -55,7 +55,7 @@ var drawMap = function(mapData, scale, data, totalData, htmlID, totalLabelID) {
         .append('title')
         .text(function(d) {
             var numPickups = 0;
-            var title = _.split(_.replace(_.replace(d.properties.NTAName, /etc-/, ""), /-/g, "/"), "/");
+            var title = _.split(_.replace(_.replace(d.properties.NTAName, /etc-/, ''), /-/g, '/'), '/');
             data.map(function(o) {
                 if (o.key == d.properties.NTACode) {
                     numPickups = o.value;
@@ -64,7 +64,49 @@ var drawMap = function(mapData, scale, data, totalData, htmlID, totalLabelID) {
             title = title.map(function(t) {
                 return _.capitalize(t);
             });
-            return _.join(title, "/") + ": " + numeral(numPickups).format();
+            return _.join(title, '/') + ': ' + numeral(numPickups).format();
+        });
+
+}
+
+var drawBar = function(data, htmlID) {
+
+    var bWidth = 315,
+        bHeight = 150,
+        midX = 200;
+
+    var barSvg = d3.select(htmlID).append('svg')
+        .attr('width', bWidth)
+        .attr('height', bHeight)
+        .style('vertical-align', 'top');
+
+    var y = d3.scaleBand().padding(0.1).range([0, bHeight]).domain(data.map(function(d) {
+        return d.name; }));
+    var x = d3.scaleLinear().range([0, bWidth - midX]).domain([0, d3.max(data, function(d) {
+        return d.value; })]);
+
+    var bars = barSvg.selectAll('.bar').data(data)
+        .enter().append('g')
+        .attr('transform',
+            function(d) {
+                return 'translate(-100,' + y(d.name) + ')'; })
+        .attr('class', 'bar');
+
+    bars.append('rect')
+        .attr('x', midX)
+        .attr('y', 0)
+        .attr('width', function(d) {
+            return x(d.value); })
+        .classed('highlight', function(d) {
+            return d.name == 'Jan 2015'; })
+        .attr('height', y.bandwidth());
+
+    bars.append('text')
+        .attr('x', midX - 4)
+        .attr('y', 12)
+        .style('text-anchor', 'end')
+        .text(function(d) {
+            return d.name;
         });
 
 }
@@ -76,37 +118,58 @@ var dataViz = function(errors, mapData, fhvBases, zones,
 
     if (errors) throw errors;
 
-    var uber = countValues(_.union(uber1, uber2, uber3, uber4, uber5, uber6), "value");
-    var fhv = countValues(_.union(fhv1, fhv2, fhv3, fhv4, fhv5, fhv6), "value");
-    var total = countValues(_.union(uber, fhv), "value");
+    var uber = countValues(_.union(uber1, uber2, uber3, uber4, uber5, uber6), 'value');
+    var fhv = countValues(_.union(fhv1, fhv2, fhv3, fhv4, fhv5, fhv6), 'value');
+    var total = countValues(_.union(uber, fhv), 'value');
+
+    /* sum by each month */
+    var ubers = [uber1, uber2, uber3, uber4, uber5, uber6];
+    var months = [
+        { 'key': 0, 'value': 'Jan 2015' },
+        { 'key': 1, 'value': 'Feb 2015' },
+        { 'key': 2, 'value': 'Mar 2015' },
+        { 'key': 3, 'value': 'Apr 2015' },
+        { 'key': 4, 'value': 'May 2015' },
+        { 'key': 5, 'value': 'Jun 2015' },
+    ];
+
+    _.each(ubers, function(d, i) {
+        var sum = {
+            'name': months[i]['value'],
+            'value': _.sumBy(d, 'value')
+        };
+        ubers[i] = sum;
+    });
 
     /*-------- design 1 --------*/
-    drawMap(mapData, 75000, uber, total, "#nyc-uber", "#totalNYCUberPickups");
+    drawMap(mapData, 75000, 0, uber, total, '#nyc-uber', '.totalNYCUberPickups');
     /*-------- /design 1 --------*/
+
+    /*-------- design 3 --------*/
+    drawMap(mapData, 75000, 315, uber, total, '#nyc-uber-bar', '.totalNYCUberPickups');
+    drawBar(ubers, '#nyc-uber-bar');
+    /*-------- /design 3 --------*/
 
     /*-------- design 2 --------*/
     var manhattan = _.filter(mapData.features, function(d) {
-        return d.properties.BoroName === "Manhattan";
+        return d.properties.BoroName === 'Manhattan';
     });
 
     mapData = {
-        "type": "FeatureCollection",
-        "features": manhattan
+        'type': 'FeatureCollection',
+        'features': manhattan
     };
 
-    drawMap(mapData, 150000, uber, total, '#manhattan-uber', "#totalMNUberPickups");
-    drawMap(mapData, 150000, fhv, total, '#manhattan-fhv', "#totalMNFHVPickups");
+    drawMap(mapData, 150000, 0, uber, total, '#manhattan-uber', '#totalMNUberPickups');
+    drawMap(mapData, 150000, 0, fhv, total, '#manhattan-fhv', '#totalMNFHVPickups');
     /*-------- /design 2 --------*/
-
-    /*-------- design 3 --------*/
-    /*-------- /design 3 --------*/
 
 }
 
 var processFHVData = function(fhv) {
 
     fhv = fhv.map(function(d) {
-        var ntacode = "";
+        var ntacode = '';
         zones.map(function(z) {
             if (z.location_id === d.locationID &&
                 z.location_id != 264 &&
@@ -114,12 +177,12 @@ var processFHVData = function(fhv) {
                 ntacode = z.ntacode;
             }
         });
-        d["ntacode"] = ntacode;
+        d['ntacode'] = ntacode;
         return d;
     });
 
     fhv = fhv.filter(function(d) {
-        if (d.locationID != "" && d.ntacode != "") {
+        if (d.locationID != '' && d.ntacode != '') {
             return d;
         }
     });
@@ -130,12 +193,12 @@ var processFHVData = function(fhv) {
 var processUberData = function(data, monthIdx) {
 
     var months = [
-        { "key": 0, "value": "January 2015" },
-        { "key": 1, "value": "February 2015" },
-        { "key": 2, "value": "March 2015" },
-        { "key": 3, "value": "April 2015" },
-        { "key": 4, "value": "May 2015" },
-        { "key": 5, "value": "June 2015" },
+        { 'key': 0, 'value': 'January 2015' },
+        { 'key': 1, 'value': 'February 2015' },
+        { 'key': 2, 'value': 'March 2015' },
+        { 'key': 3, 'value': 'April 2015' },
+        { 'key': 4, 'value': 'May 2015' },
+        { 'key': 5, 'value': 'June 2015' },
     ];
 
     var month = months[monthIdx];
@@ -147,17 +210,17 @@ var processUberData = function(data, monthIdx) {
     _.each(data, o => _.each(o, (v, k) => o[k] = v.trim()));
 
     data = data.map(function(o) {
-        var ntacode = "";
+        var ntacode = '';
         zones.map(function(z) {
             if (z.location_id === o.locationID) {
                 ntacode = z.ntacode;
             }
         });
-        o["ntacode"] = ntacode;
+        o['ntacode'] = ntacode;
         return o;
     });
 
-    var dataCount = _.orderBy(_.map(_.countBy(data, "ntacode"), function(value, key) { return { key: key, value: value }; }), "value", "desc");
+    var dataCount = _.orderBy(_.map(_.countBy(data, 'ntacode'), function(value, key) { return { key: key, value: value }; }), 'value', 'desc');
 
     return dataCount;
 }
